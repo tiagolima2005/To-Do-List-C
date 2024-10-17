99909b594e237332a50632a1a4f975a0091f5fb4
tiagolima2005 <tilima.steam@gmail.com>
2024-10-17 20:40:54 +0100

    1º menu inicial

--- Main/ToDoListCode.c
+++ Main/ToDoListCode.c
@@ -3,5 +3,11 @@
 #include <stdbool.h>
 
 int main(){
-    printf("ola");
+    printf("\nWelcome to your To do List App\n");
+    printf("Type '1' to Add a task\n");
+    printf("Type '2' to Remove a task\n");
+    printf("Type '3' to Edit a task\n");
+    printf("Type '4' to Show all your tasks\n");
+    printf("Type '5' to Mark a task as completed\n");
+    printf("Type '6' to Exit the program\n");
 }
